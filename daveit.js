ff27ac570f181464770c199482a837d30e101940
Labodi David <labodidavid@gmail.com>
2019-07-11 15:40:23 +0200

    Update - 1.0.1

---
+++ daveit.js
@@ -0,0 +1,7 @@
+//DaveIT Services - Configuration JS -- https://daveitservices.tk
+exports.BOTNAME = 'DaveBOT';
+exports.DCSERVNAME = 'DaveIT Services';
+exports.WELCOMEMSG = `Üdvözöllek a ${DCSERVNAME} Discord szerverén, ${member}! \n${BOTNAME} parancsok listája: ${prefix}help`;
+exports.HELP = `${BOTNAME} Parancsok (Előtag: ${PREFIX}) : \nhelp - Parancsok listája\nping - BOT válaszideje a Discord szervereihez\n(Előtag nélkül)dprefix - ${BOTNAME} előtagjának kiíratása`;
+exports.PING = `${BOTNAME} pingje a Discord szerverhez: ${client.ping} ms`;
+exports.BOTVERSION = '1.0.1';
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Discord = require('discord.js');
 	// google_api_key,
 // } = require('./config.json');
 const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
+const { BOTNAME, BOTVERSION, DCSERVNAME, WELCOMEMSG, HELP, PING } = require('./daveit');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 const client = new Discord.Client();
@@ -12,14 +13,14 @@ const client = new Discord.Client();
 
 //ALLAPOT UZENETEK KONZOL
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- console.log(`DaveBOT STARTED RUNNING!`);
+ console.log(`Felcsatlakozva mint: ${client.user.tag}!`);
+ console.log(`${BOTNAME} Elkezdett uzemelni!`);
  });
 client.once('reconnecting', () => {
  console.log('Ujracsatlakozas!');
 });
-client.on('disconnect', () => console.log('DaveBOT Lecsatlakozik.'));
-client.on('reconnecting', () => console.log('DaveBOT Ujracsatlakozik.'));
+client.on('disconnect', () => console.log(`${BOTNAME} Lecsatlakozik.`));
+client.on('reconnecting', () => console.log(`${BOTNAME} Ujracsatlakozik.`));
 client.on('warn', console.warn);
 client.on('error', console.error);
 //
@@ -29,34 +30,29 @@ client.on('message', msg => {
   if (!msg.content.startsWith(PREFIX) || msg.author.bot) return;
  
   if (msg.content.startsWith(PREFIX + "ping")) {
-    msg.reply(`DaveBOT pingje a Discord szerverhez: ${client.ping} ms`);
+    msg.reply(`${PING}`);
   } else
   if (msg.content.startsWith(PREFIX + "help")) {
-    msg.channel.send(`DaveBOT Parancsok (Előtag: ${PREFIX}) : \nhelp - Parancsok listája\nping - BOT válaszideje a Discord szervereihez\n(Előtag nélkül)dprefix - DaveBOT előtagjának kiíratása`);
+    msg.channel.send(`${HELP}`);
   }
 });
 
  client.on('message', msg => {
  if (msg.content === 'dprefix') {
- msg.reply(`DaveBOT parancsok prefixe: ${PREFIX}`);
+ msg.reply(`${BOTNAME} parancsok prefixe: ${PREFIX}`);
  }
  });
 client.on('message', async message => {
   if (message.author.bot) return;
   if (!message.content.startsWith(PREFIX)) return;
 });
-//MUSICBOT
-//
+
  
 // WELCOME MSG
 client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
   const channel = member.guild.channels.find(ch => ch.name === 'welcome');
-  // Do nothing if the channel wasn't found on this server
   if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`Üdvözöllek a DaveIT Services Discord szerverén, ${member}! \nDaveBOT parancsok listája: ${prefix}help`);
+  channel.send(`${WELCOMEMSG}`);
 });
 //
 client.login(TOKEN);
-// client.login('NTk4NDkxMTQ4OTk4Mjc5MjIw.XSXa1g.87MmrjvoQN57g16CV7_Df7XgP8Y');
